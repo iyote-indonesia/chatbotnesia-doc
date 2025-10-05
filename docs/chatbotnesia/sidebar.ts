9a2d307ad3124d57d86af8629abfba092c8f2921
Budi Irwan Firmansyah <maika.kanaka@gmail.com>
2025-10-05 23:09:59 +0700

    First commit "chatbotnesia-api"

---
+++ docs/chatbotnesia/sidebar.ts
@@ -0,0 +1,90 @@
+import type { SidebarsConfig } from "@docusaurus/plugin-content-docs";
+
+const sidebar: SidebarsConfig = {
+  apisidebar: [
+    {
+      type: "doc",
+      id: "chatbotnesia/chatbot-ai-server",
+    },
+    {
+      type: "category",
+      label: "broadcast/whatsapp",
+      items: [
+        {
+          type: "doc",
+          id: "chatbotnesia/get-webhook-logs-broadcast-whatsapp-webhook-logs-get",
+          label: "Get Webhook Logs",
+          className: "api-method get",
+        },
+        {
+          type: "doc",
+          id: "chatbotnesia/get-broadcast-list-broadcast-whatsapp-logs-get",
+          label: "Get Broadcast List",
+          className: "api-method get",
+        },
+        {
+          type: "doc",
+          id: "chatbotnesia/send-message-broadcast-whatsapp-send-message-post",
+          label: "Send Message",
+          className: "api-method post",
+        },
+        {
+          type: "doc",
+          id: "chatbotnesia/send-message-attachment-broadcast-whatsapp-send-attachment-post",
+          label: "Send Message Attachment",
+          className: "api-method post",
+        },
+        {
+          type: "doc",
+          id: "chatbotnesia/get-device-broadcast-whatsapp-device-client-key-get",
+          label: "Get Device",
+          className: "api-method get",
+        },
+        {
+          type: "doc",
+          id: "chatbotnesia/webhook-tracking-broadcast-whatsapp-webhook-tracking-post",
+          label: "WhatsApp Message Tracking Webhook",
+          className: "api-method post",
+        },
+        {
+          type: "doc",
+          id: "chatbotnesia/webhook-incoming-broadcast-whatsapp-webhook-incoming-post",
+          label: "WhatsApp Incoming Message Webhook",
+          className: "api-method post",
+        },
+      ],
+    },
+    {
+      type: "category",
+      label: "broadcast/whatsapp-business",
+      items: [
+        {
+          type: "doc",
+          id: "chatbotnesia/send-template-text-broadcast-whatsapp-bs-send-template-text-post",
+          label: "Send Template Text",
+          className: "api-method post",
+        },
+        {
+          type: "doc",
+          id: "chatbotnesia/send-message-template-media-broadcast-whatsapp-bs-send-template-media-post",
+          label: "Send Message Template Media",
+          className: "api-method post",
+        },
+      ],
+    },
+    {
+      type: "category",
+      label: "UNTAGGED",
+      items: [
+        {
+          type: "doc",
+          id: "chatbotnesia/root-get",
+          label: "Cek status server",
+          className: "api-method get",
+        },
+      ],
+    },
+  ],
+};
+
+export default sidebar.apisidebar;
